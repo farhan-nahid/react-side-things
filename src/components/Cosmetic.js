df222e8e31efb489a95f7f49c892d0536ada754a
gesora96 <nahidahmed9933@gmail.com>
2021-10-10 00:47:37 +0600

    first commit

---
+++ src/components/Cosmetic.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { addToDb, deleteFromDb } from "../utilites/fakedb";
+
+const Cosmetic = (props) => {
+  const { name, price, gender, _id } = props.cosmetic;
+  const handlePurchase = (id) => {
+    //set to local stoage
+    console.log(id);
+    addToDb(id);
+  };
+
+  const handleRemove = (id) => {
+    deleteFromDb(id);
+  };
+
+  return (
+    <div>
+      <h2>name: {name}</h2>
+      <p>
+        _id: {_id} Price: ${price}
+      </p>
+      <p>
+        <small>gender: {gender}</small>
+      </p>
+      <button onClick={() => handlePurchase(_id)}>Purchase</button>
+      <button onClick={() => handleRemove(_id)}>Remove</button>
+    </div>
+  );
+};
+
+export default Cosmetic;
